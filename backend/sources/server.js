d9fc947f74bfe407ab4db9910a100fef3638f6db
Destanto <destantomy8@gmail.com>
2024-03-17 20:02:44 +0700

    vercel.json config

--- backend/sources/server.js
+++ backend/sources/server.js
@@ -37,7 +37,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Serve images from the 'upload' directory
 app.use('/images', express.static('sources/upload'));
-
 // routes
 app.use('/api/auth/', loginRoutes);
 app.use('/api/endpoint/home', getCountRoutes);
